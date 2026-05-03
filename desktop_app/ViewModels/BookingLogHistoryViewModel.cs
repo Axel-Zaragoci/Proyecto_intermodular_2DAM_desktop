c382067b65dc13a77e7cb869bac2662ab5b837e8
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-05-03 19:42:32 +0200

    Creo vista y navegación para los logs

---
+++ desktop_app/ViewModels/BookingLogHistoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace desktop_app.ViewModels;
+
+public class BookingLogHistoryViewModel : ViewModelBase
+{
+    private static BookingLogHistoryViewModel? _instance;
+    public static BookingLogHistoryViewModel Instance => _instance ??= new BookingLogHistoryViewModel();
+    
+    private string _bookingId = "";
+    public string BookingId
+    {
+        get => _bookingId;
+        set
+        {
+            _bookingId = value;
+            Console.WriteLine(_bookingId);
+        } 
+    }
+}
