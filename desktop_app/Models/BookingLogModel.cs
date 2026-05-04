dac4d6d69d33e12ffd3d9e9c590c3fdcad3afa7b
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-05-04 09:37:51 +0200

    Creo el modelo de los logs de reserva

---
+++ desktop_app/Models/BookingLogModel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace desktop_app.Models;
+
+public class BookingLogModel
+{
+    [JsonProperty ("collections")]
+    public string Collections { get; set; }
+    
+    [JsonProperty("action")]
+    public string Action { get; set; }
+    
+    [JsonProperty ("documentId")]
+    public string DocumentId { get; set; }
+    
+    [JsonProperty ("userId")]
+    public string UserId { get; set; }
+    
+    [JsonProperty ("createdAt")]
+    public DateTime CreatedAt { get; set; }
+    
+    [JsonProperty ("oldBooking")]
+    public BookingModel OldBooking { get; set; }
+    
+    [JsonProperty ("newBooking")]
+    public BookingModel NewBooking { get; set; }
+}
