190b8823e940134e1d14df8287c8f34592adf11b
Axel-Zaragoci <axelzaragoci@gmail.com>
2026-05-17 17:42:49 +0200

    Estandarizo margen en los campos de paginación y cambio selector de imagen de perfil de usuario

--- desktop_app/ViewModels/User/UserUtilitiesViewModel.cs
+++ desktop_app/ViewModels/User/UserUtilitiesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using desktop_app.Models;
 using System.Windows.Input;
 using desktop_app.Services;
 using System.Text.RegularExpressions;
+using Microsoft.Win32;
 
 namespace desktop_app.ViewModels.User
 {
@@ -24,7 +26,7 @@ namespace desktop_app.ViewModels.User
 
         public ICommand SaveCommand { get; }
         public ICommand CancelCommand { get; }
-
+        
         private string? _errorMessage;
         public string? ErrorMessage
         {
@@ -43,9 +45,16 @@ namespace desktop_app.ViewModels.User
                 VipStatus = false
             };
 
+            if (!string.IsNullOrEmpty(User.ImageRoute))
+            {
+                ImageRoute = ApiService.BaseUrl.Substring(0, ApiService.BaseUrl.Length - 1) + User.ImageRoute;
+            }
+            
             SaveCommand = new AsyncRelayCommand(SaveAsync, () => Mode != UserUtilitiesMode.View);
             CancelCommand = new RelayCommand<object>(_ => Cancel());
+            PickImageCommand = new RelayCommand(_ => PickImageLocal());
         }
+        
         private String Validate()
         {
             if (string.IsNullOrWhiteSpace(User.FirstName))
@@ -100,6 +109,17 @@ namespace desktop_app.ViewModels.User
                 ErrorMessage = err;
                 return;
             }
+            
+            if (!string.IsNullOrWhiteSpace(_imagePath))
+            {
+                var uploaded = await ImageService.UploadSingleAsync(_imagePath);
+                if (uploaded == null)
+                {
+                    ErrorMessage = "No se puedo subir la imagen";
+                    return;
+                }
+                User.ImageRoute = uploaded.Url;
+            }
 
             try
             {
@@ -115,6 +135,51 @@ namespace desktop_app.ViewModels.User
         private void Cancel()
         {
             NavigationService.Instance.NavigateTo<desktop_app.Views.UserView>();
+        }
+        
+        
+        
+        
+        
+        
+        private string _imageLabel = "Sin seleccionar";
+        private string _imagePath = string.Empty;
+        
+        private string _imageRoute = string.Empty;
+        public string ImageRoute
+        {
+            get => _imageRoute;
+            set
+            {
+                SetProperty(ref _imageRoute, value);
+                OnPropertyChanged();
+            }
+        }
+        
+        public string ImageLabel
+        {
+            get => _imageLabel;
+            set => SetProperty(ref _imageLabel, value);
+        }
+        
+        public ICommand PickImageCommand { get; }
+
+        private void PickImageLocal()
+        {
+            Console.WriteLine("PickImage ejecutado");
+            
+            var dlg = new OpenFileDialog
+            {
+                Filter = "Imágenes|*.jpg;*.jpeg;*.png;*.webp;*.gif",
+                Multiselect = false
+            };
+
+            if (dlg.ShowDialog() != true) return;
+
+            var path = dlg.FileName;
+            _imagePath = path;
+            ImageLabel = Path.GetFileName(path);
+            ImageRoute = path;
         }
     }
 }
